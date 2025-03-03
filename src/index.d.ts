359d62a0447e9d6008029c166d17a6a71cae913e
anupam-lyzr <anupam@lyzr.com>
2025-03-03 13:39:31 +0530

    Updated the type defination for the new implimentation

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,17 @@
 declare module 'lyzr-agent' {
+
+  interface LyzrAgentInitConfig {
+    pagosUrl?: string;
+    agentStudioUrl?: string;
+    agentStudioUrlSignup?: string;
+  }
   class LyzrAgent {
-    init(publicKey: string): Promise<LyzrAgent>;
+    /**
+   * Initializes the agent.
+   * @param publicKey Optional public key to override the one provided in the constructor.
+   * @param config Optional configuration object to override URLs.
+   */
+    init(publicKey?: string, config?: LyzrAgentInitConfig): Promise<LyzrAgent>;
     setBadgePosition(x?: string, y?: string): void;
     getKeys(): Promise<string[]>;
     getKeysUser(): Promise<any>;
